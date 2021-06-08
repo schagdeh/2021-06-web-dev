5b473ff647ee5649e298105f9747d5689b4cb07e
schagdeh <40886637+schagdeh@users.noreply.github.com>
2021-06-08 14:53:34 +0200

    state definition change, render function to render array of default objects

--- 02-typescript/02-to-do-list/dist/bundle.js
+++ 02-typescript/02-to-do-list/dist/bundle.js
@@ -14,20 +14,36 @@
 /*!*********************!*\
   !*** ./src/main.ts ***!
   \*********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\nconst toDos = [\n    { title: 'TypeScript', done: true },\n    { title: 'ReactJS', done: true },\n    { title: 'NodeJS', done: false },\n];\n\n\n//# sourceURL=webpack://to-do-list/./src/main.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\nconst appDiv = document.getElementById('app');\nif (!(appDiv instanceof HTMLDivElement)) {\n    throw new Error('No element with Id \"app\" found');\n}\n;\nconst toDoes = [\n    { title: 'TypeScript', done: true },\n    { title: 'ReactJS', done: true },\n    { title: 'NodeJS', done: false },\n    { title: 'ExpressJS', done: false },\n];\nconst render = () => {\n    appDiv.innerHTML = \"\";\n    const toDoUl = document.createElement('ul');\n    toDoUl.setAttribute('class', 'ulStyle');\n    for (const toDo of toDoes) {\n        const toDoLi = document.createElement('li');\n        toDoLi.setAttribute('class', 'listStyle');\n        const p = document.createElement('p');\n        p.setAttribute('class', 'pStyle');\n        const span = document.createElement('span');\n        toDoLi.appendChild(p);\n        toDoLi.appendChild(span);\n        p.textContent = toDo.title;\n        span.textContent = toDo.done.toString();\n        toDoUl.appendChild(toDoLi);\n        console.log(toDoLi);\n    }\n    appDiv.appendChild(toDoUl);\n};\nrender();\n\n\n\n//# sourceURL=webpack://to-do-list/./src/main.ts?");
 
 /***/ })
 
 /******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/main.ts"]();
+/******/ 	__webpack_modules__["./src/main.ts"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- 02-typescript/02-to-do-list/src/main.ts
+++ 02-typescript/02-to-do-list/src/main.ts
@@ -1,10 +1,47 @@
+import { NONAME } from "dns"
+
+const appDiv = document.getElementById('app')
+
+
+if (!(appDiv instanceof HTMLDivElement)) {
+  throw new Error('No element with Id "app" found')
+};
+
 type State = {
   title: string;
   done: boolean;
-}
+}[];
 
-const toDos: State[] = [
+const toDoes: State = [
   {title: 'TypeScript', done: true},
   {title: 'ReactJS', done: true},
   {title: 'NodeJS', done: false},
+  {title: 'ExpressJS', done: false},
+
 ] 
+
+const render = () => {
+  appDiv.innerHTML = "";
+
+  const toDoUl = document.createElement('ul');
+  toDoUl.setAttribute('class', 'ulStyle')
+  
+
+  for(const toDo of toDoes) {
+    const toDoLi = document.createElement('li');
+    toDoLi.setAttribute('class', 'listStyle')
+    const p = document.createElement('p');
+    p.setAttribute('class', 'pStyle')
+    const span = document.createElement('span');
+    toDoLi.appendChild(p)
+    toDoLi.appendChild(span)
+    p.textContent = toDo.title
+    span.textContent = toDo.done.toString()
+    toDoUl.appendChild(toDoLi)
+    console.log(toDoLi);
+
+  }
+  appDiv.appendChild(toDoUl)
+}; 
+
+render();
