a862f6289ba9df0e0113110b732c56f88262bcb5
schagdeh <40886637+schagdeh@users.noreply.github.com>
2021-06-14 13:54:31 +0200

    convert celsius to fahrenheit

---
+++ 03-react/04-converter/src/main.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const appDiv = document.getElementById("app");
+
+if (!(appDiv instanceof HTMLDivElement)) {
+	throw new Error("No div with id 'app' found");
+}
+
+/**
+ * Convert a string to an integer.
+ * 
+ * Returns 0 if the string can not be converted.
+ */
+const toInt = (value: string): number => {
+	const result = Number(value);
+	return isNaN(result) ? 0 : result;
+};
+
+const App = (): JSX.Element => {
+	const [a, setA] = React.useState("");
+	const [b, setB] = React.useState("");
+	console.log(a);
+	
+	
+	const fToc = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setA(e.target.value)
+		const celsius = ((toInt(e.target.value) - 32) * 5/9).toFixed(2).toString()
+		setB(celsius)
+		e.target.value === '' && setB('')
+		 return 
+	}
+	const cTof = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setB(e.target.value)
+		const celsius = ((toInt(e.target.value) * 9/5) + 32).toFixed(2).toString()
+		 setA(celsius)
+		 e.target.value === '' && setA('')
+		 return 
+	}
+
+	return (
+		<main>
+			<input
+				type="number"
+				value={a}
+				placeholder="°F"
+				onChange={(e) => fToc(e)}
+			/>
+			{" + "}
+			<input
+				type="number"
+				value={b}
+				placeholder="°C"
+				onChange={(e) => cTof(e)}
+			/>
+			{/* {" = "}
+			<span>{toInt(a) + toInt(b)}</span> */}
+		</main>
+	);
+};
+
+ReactDOM.render(<App />, appDiv);
