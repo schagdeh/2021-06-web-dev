42900d5d19528e39ac904f33fb2e7c4633d3eac1
schagdeh <40886637+schagdeh@users.noreply.github.com>
2021-06-15 08:17:50 +0200

    image search API exercise done

---
+++ 03-react/05-images-search/src/main.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { useState } from "react";
+import * as ReactDOM from "react-dom";
+
+const appDiv = document.getElementById("app");
+
+if (!(appDiv instanceof HTMLDivElement)) {
+  throw new Error("No div with id 'app' found");
+}
+
+interface Images {
+  id: number;
+  webformatURL: string;
+}
+
+const App = (): JSX.Element => {
+  const [images, setImages] = useState<Images[] | null>(null);
+  const [value, setValue] = useState("");
+  const [text, setText] = useState("Type something to search for photos");
+
+  const IMAGE_URL = new URL(
+    "https://pixabay.com/api/?key=22076185-8c3db306e8ad607345e161e46"
+  );
+  IMAGE_URL.searchParams.append("q", value);
+  IMAGE_URL.searchParams.append("per_page", "10");
+  IMAGE_URL.searchParams.append("image_type", "photo");
+
+  const fetchImages = async () => {
+    const response = await fetch(IMAGE_URL.toString());
+    const json = await response.json();
+    const allImages = json.hits;
+    setImages(allImages);
+    setText("There is no matching photo found!");
+  };
+
+  return (
+    <main>
+      <section className="searchSection">
+        <div className="searchBoxDiv">
+          <div>
+            <input type="search" id="search" placeholder="Search..." onChange={(e) => setValue(e.target.value)}/>
+            <button onClick={fetchImages}>Search</button>
+          </div>
+        </div>
+      </section>
+      <section className="imageResultSection">
+        <div className="imageContainer">
+          {!images || images.length === 0 ? (
+            <h4 className="infoText">{text}</h4>
+          ) : (
+            images.map((image) => {
+              return (
+				  <div className="imageDivLi">
+					<li key={image.id}>
+						<img src={image.webformatURL} alt="" />
+					</li>
+				  </div>
+              );
+            })
+          )}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+ReactDOM.render(<App />, appDiv);
